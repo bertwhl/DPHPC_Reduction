e6df778a04e2398dc3d68430b33aecc4e96f9304
hazxel <steven.b.zhou@foxmail.com>
2021-12-02 15:05:52 +0100

    loop version of one block per row

--- library/mwpr.py
+++ library/mwpr.py
@@ -21,6 +21,24 @@ def AB_MWPR_1(inputs: dace.float64[H, W], wn: dace.int64):
     return outputs
 
 @dace.program
+def AB_MWPR_LOOP_1(inputs: dace.float64[H, W], ln: dace.int64):
+    outputs = dace.ndarray([H], dtype=dace.float64)
+    outputs[:] = 0
+    for block_id in dace.map[0:H]:
+        shared = dace.ndarray([32,32], dtype=dace.float64, storage=dace.StorageType.GPU_Shared)
+        for warp_id, thread_id in dace.map[0:32, 0:32]:
+            value = dace.float64(0)
+            for i in dace.map[0:ln]:
+                col = i * 1024 + warp_id * 32 + thread_id
+                value += inputs[block_id, col]
+            shared[warp_id, thread_id] = value
+        for warp_id, thread_id in dace.map[0:32, 0:32]:
+            reduced = warpReduce_sum(shared[warp_id, thread_id])
+            if thread_id == 0:
+                outputs[block_id] += reduced
+    return outputs
+
+@dace.program
 def AB_MWPR_2(inputs: dace.float64[H, W], bn: dace.int64):
     outputs = dace.ndarray([H], dtype=dace.float64)
     outputs[:] = 0
@@ -55,45 +73,68 @@ def AB_MWPR_3(inputs: dace.float64[H, W], bn: dace.int64, ln: dace.int64, rpb: d
                 outputs[block_id * rpb + warp_id_x] += reduced[warp_id_x]
     return outputs
 
+def test_1():
+    sdfg1 = AB_MWPR_1.to_sdfg()
+    sdfg1.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+    
+    warp_num = w // 32 # w should be 1~32
+ 
+    a1 = np.random.rand(h, w)
+    b1 = sdfg1(H = h, W = w, inputs = a1, wn = warp_num)
+    c1 = np.sum(a1, axis=1)
+    assert np.allclose(b1, c1)
+
+def test_1_loop():
+    sdfg = AB_MWPR_LOOP_1.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    loop_num = w // 1024
+
+    a = np.random.rand(h, w)
+    b = sdfg(H = h, W = w, inputs = a, ln = loop_num)
+    c = np.sum(a, axis=1)
+    assert np.allclose(b, c)
+
+def test_2():
+    sdfg2 = AB_MWPR_2.to_sdfg()
+    sdfg2.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    block_num = h * w // 512
+
+    a2 = np.random.rand(h, w)
+    b2 = sdfg2(H = h, W = w, inputs = a2, bn = block_num)
+    c2 = np.sum(a2, axis=1)
+    assert np.allclose(b2, c2)
+
+def test_3():
+    sdfg3 = AB_MWPR_3.to_sdfg()
+    sdfg3.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    row_per_block = 2
+    warp_per_row = 32 // row_per_block
+    block_num = h // row_per_block
+    loop_num = w * row_per_block // 1024
+
+    a3 = np.random.rand(h, w)
+    b3 = sdfg3(H = h, W = w, inputs = a3, bn = block_num, ln = loop_num, rpb = row_per_block, wpr = warp_per_row)
+    c3 = np.sum(a3, axis=1)
+    assert np.allclose(b3, c3)
+
 if __name__ == '__main__':
     if len(sys.argv) != 4:
-        raise Exception('only accept 3 argument')
+        raise Exception('only accept 3 arguments')
     test_case = sys.argv[1]
     h, w = int(sys.argv[2]), int(sys.argv[3])
-    if test_case ==  "1":
-        sdfg1 = AB_MWPR_1.to_sdfg()
-        sdfg1.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
-        
-        warp_num = w // 32 # w should be 1~32
- 
-        a1 = np.random.rand(h, w)
-        b1 = sdfg1(H = h, W = w, inputs = a1, wn = warp_num)
-        c1 = np.sum(a1, axis=1)
-        assert np.allclose(b1, c1)
-
-    elif test_case == "2":
-        sdfg2 = AB_MWPR_2.to_sdfg()
-        sdfg2.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
-
-        block_num = h * w // 512
-
-        a2 = np.random.rand(h, w)
-        b2 = sdfg2(H = h, W = w, inputs = a2, bn = block_num)
-        c2 = np.sum(a2, axis=1)
-        assert np.allclose(b2, c2)
-
-    elif test_case == "3":
-        sdfg3 = AB_MWPR_3.to_sdfg()
-        sdfg3.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
-
-        row_per_block = 16
-        warp_per_row = 32 // row_per_block
-        block_num = h // row_per_block
-        loop_num = w * row_per_block // 1024
-
-        a3 = np.random.rand(h, w)
-        b3 = sdfg3(H = h, W = w, inputs = a3, bn = block_num, ln = loop_num, rpb = row_per_block, wpr = warp_per_row)
-        c3 = np.sum(a3, axis=1)
-        assert np.allclose(b3, c3)
-    else:
-        raise Exception('invalid case number, only accept 1,2,3')
+    
+    for i in range(16):
+        if test_case ==  "1":
+            if w > 1024:
+                test_1_loop()
+            else:
+                test_1()
+        elif test_case == "2":
+            test_2()
+        elif test_case == "3":
+            test_3()
+        else:
+            raise Exception('invalid case number, only accept 1, 2, 3 and all')
