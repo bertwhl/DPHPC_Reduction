03f973810e666f4de1ac7ae9b5ecb2921b29d7bf
haoranzhao1998 <32390614+haoranzhao1998@users.noreply.github.com>
2021-12-02 14:15:29 +0100

    Update Reduce3D.py

--- library/Reduce3D.py
+++ library/Reduce3D.py
@@ -33,6 +33,41 @@ def ColReduce(inputs: dace.float64[X, H, W], gridDim_x: dace.int64, gridDim_y: d
                 outputs[levelIdx,colIdx] += value
     return outputs
 
+# use warp read warp reduce for narrow corner cases
+@dace.program
+def ColReduceNarrow(inputs: dace.float64[X, H, W], gridDim_x: dace.int64, loopNum: dace.int64):
+    # define and initialize the output vector
+    outputs = dace.ndarray([X,W], dtype=dace.float64)
+    outputs[:] = 0
+    # blocks mapping
+    for blockIdx_y, blockIdx_x in dace.map[0:X, 0:gridDim_x]:
+        shared = dace.ndarray([1024], dtype=dace.float64, storage=dace.StorageType.GPU_Shared)
+        for threadIdx_y, threadIdx_x in dace.map[0:W, 0:32]:
+            # initialize value
+            value = dace.float64(0)
+            # calculate the indexs
+            threadIdx = 32*threadIdx_y + threadIdx_x
+            colIdx = threadIdx%W
+            rowIdx_offset = dace.int64(threadIdx/W)
+            rowIdx = 32*blockIdx_x + rowIdx_offset
+            rowIdx_delta = 32*gridDim_x
+            for loopIdx in dace.map[0:loopNum]:
+                # add the value
+                if rowIdx<H:
+                    value += inputs[blockIdx_y, rowIdx, colIdx]
+                rowIdx += rowIdx_delta
+            # write the value into shared memory
+            shared[W*rowIdx_offset + colIdx] = value
+        # synchronize here
+        for threadIdx_y, threadIdx_x in dace.map[0:W, 0:32]:
+            rowIdx = threadIdx_x
+            colIdx = threadIdx_y
+            # warp reduce
+            reduced = warpReduce_sum(shared[W*rowIdx + colIdx])
+            # write back to global memory with atomic add
+            if threadIdx_x==0:
+                outputs[blockIdx_y,colIdx] += reduced
+    return outputs
 
 #B1-A1-B2 -> A1
 #using more blocks + atomic add
@@ -70,8 +105,22 @@ if __name__ == '__main__':
         compared = np.sum(inputs, axis=1)
         assert np.allclose(outputs, compared)
         print("test 3D column reduce successfully")
+    if test == 'colreduce_narrow':
+        sdfg = ColReduceNarrow.to_sdfg()
+        sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
 
-    if test == 'rowreduce':
+        x = 8
+        h = 8192
+        w = 12
+
+        # Test
+        inputs = np.random.rand(x, h, w)
+        outputs = sdfg(X=x, H=h, W=w, inputs=inputs, gridDim_x=256, loopNum=1)
+        compared = np.sum(inputs, axis=1)
+        assert np.allclose(outputs, compared)
+        print("test 3D col reduce narrow successfully")
+        
+    if test == 'rowreduce_narrow':
         sdfg = RowReduceNarrow.to_sdfg()
         sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
 
@@ -85,4 +134,4 @@ if __name__ == '__main__':
         compared = np.sum(inputs, axis=(0,2))
         assert np.allclose(outputs, compared)
 
-        print("test 3D row reduce successfully")
+        print("test 3D row reduce narrow successfully")
