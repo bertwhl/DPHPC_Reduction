b664d50e2c8f98c39389c3e63765f635ca1fa414
haoranzhao1998 <32390614+haoranzhao1998@users.noreply.github.com>
2021-12-02 09:58:30 +0100

    Update Reduce3D.py

--- library/Reduce3D.py
+++ library/Reduce3D.py
@@ -5,7 +5,37 @@ H = dace.symbol('H')
 W = dace.symbol('W')
 X = dace.symbol('X')
 
+#A1-B1-A2 -> A1-A2
+#using more blocks
+@dace.program
+def ColReduce(inputs: dace.float64[X, H, W], gridDim_x: dace.int64, gridDim_y: dace.int64, blockDim_x: dace.int64, loopNum: dace.int64):
+    # define and initialize the output vector
+    outputs = dace.ndarray([X,W], dtype=dace.float64)
+    outputs[:] = 0
+    # blocks mapping
+    for blockIdx_y, blockIdx_x in dace.map[0:gridDim_y, 0:gridDim_x]:
+        # thread mapping
+        for threadIdx_x in dace.map[0:blockDim_x]:
+            # initialize value
+            value = dace.float64(0)
+            # calculate the indexes
+            levelIdx = blockIdx_y // H
+            rowIdx = dace.int64(blockIdx_y % H)
+            colIdx = blockDim_x * blockIdx_x + threadIdx_x
+            # sum up the values
+            if colIdx<W:
+                delta = gridDim_y
+                for loopIdx in dace.map[0:loopNum]:
+                    if rowIdx<H:
+                        value += inputs[levelIdx, rowIdx, colIdx]
+                    rowIdx += delta
+                # write back to global memory with atomic add
+                outputs[levelIdx,colIdx] += value
+    return outputs
+
+
 #B1-A1-B2 -> A1
+#using more blocks + atomic add
 @dace.program
 def RowReduceNarrow(inputs: dace.float64[X, H, W], gridDim_y: dace.int64, gridDim_x: dace.int64, blockDim_x: dace.int64):
     outputs = dace.ndarray([H], dtype=dace.float64)
@@ -23,16 +53,36 @@ def RowReduceNarrow(inputs: dace.float64[X, H, W], gridDim_y: dace.int64, gridDi
 if __name__ == '__main__':
     import numpy as np
     from dace.transformation.interstate import GPUTransformSDFG
-    # Transform to GPU, keep thread-block map
-    sdfg = RowReduceNarrow.to_sdfg()
-    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+    test = 'colreduce'
 
-    x = 12
-    h = 1000
-    w = 12
 
-    # Test
-    inputs = np.random.rand(x, h, w)
-    outputs = sdfg(X=x, H=h, W=w, inputs=inputs, gridDim_y=12, gridDim_x=4, blockDim_x=256)
-    compared = np.sum(inputs, axis=(0,2))
-    assert np.allclose(outputs, compared)
+    if test == 'colreduce':
+        sdfg = ColReduce.to_sdfg()
+        sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+        x = 12
+        h = 1000
+        w = 12
+
+        # Test
+        inputs = np.random.rand(x, h, w)
+        outputs = sdfg(X=x, H=h, W=w, inputs=inputs, gridDim_x=1, gridDim_y=12000, blockDim_x=12, loopNum=1)
+        compared = np.sum(inputs, axis=1)
+        assert np.allclose(outputs, compared)
+        print("test 3D column reduce successfully")
+
+    if test == 'rowreduce':
+        sdfg = RowReduceNarrow.to_sdfg()
+        sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+        x = 12
+        h = 1000
+        w = 12
+
+        # Test
+        inputs = np.random.rand(x, h, w)
+        outputs = sdfg(X=x, H=h, W=w, inputs=inputs, gridDim_y=12, gridDim_x=4, blockDim_x=256)
+        compared = np.sum(inputs, axis=(0,2))
+        assert np.allclose(outputs, compared)
+
+        print("test 3D row reduce successfully")
