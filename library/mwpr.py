30a4cbdeedd614662eddd534ab7fc5a285dd19a0
hazxel <steven.b.zhou@foxmail.com>
2021-12-08 09:59:28 +0100

    update multiple-warps-per-row

--- library/mwpr.py
+++ library/mwpr.py
@@ -8,6 +8,7 @@ from util import *
 H = dace.symbol('H')
 W = dace.symbol('W')
 
+# slow, deprecated
 @dace.program
 def AB_MWPR_1(inputs: dace.float64[H, W], wn: dace.int64):
     outputs = dace.ndarray([H], dtype=dace.float64)
@@ -20,38 +21,40 @@ def AB_MWPR_1(inputs: dace.float64[H, W], wn: dace.int64):
                 outputs[block_id] += reduced
     return outputs
 
+# slow, deprecated
+@dace.program
+def AB_MWPR_1_special(inputs: dace.float64[H, W]):
+    outputs = dace.ndarray([H], dtype=dace.float64)
+    outputs[:] = 0
+    for block_id in dace.map[0:H]:
+        for thread_id in dace.map[0:32]:
+            col = thread_id
+            if col<W:
+                value = inputs[block_id, col]
+            else:
+                value = 0
+            reduced = warpReduce_sum(value)
+            if thread_id == 0:
+                outputs[block_id] += reduced
+    return outputs
+
+# same as "one block per row"
 @dace.program
 def AB_MWPR_LOOP_1(inputs: dace.float64[H, W], ln: dace.int64):
     outputs = dace.ndarray([H], dtype=dace.float64)
     outputs[:] = 0
     for block_id in dace.map[0:H]:
-        shared = dace.ndarray([32,32], dtype=dace.float64, storage=dace.StorageType.GPU_Shared)
         for warp_id, thread_id in dace.map[0:32, 0:32]:
             value = dace.float64(0)
             for i in dace.map[0:ln]:
                 col = i * 1024 + warp_id * 32 + thread_id
                 value += inputs[block_id, col]
-            shared[warp_id, thread_id] = value
-        for warp_id, thread_id in dace.map[0:32, 0:32]:
-            reduced = warpReduce_sum(shared[warp_id, thread_id])
+            reduced = warpReduce_sum(value)
             if thread_id == 0:
                 outputs[block_id] += reduced
     return outputs
 
-@dace.program
-def AB_MWPR_2(inputs: dace.float64[H, W], bn: dace.int64):
-    outputs = dace.ndarray([H], dtype=dace.float64)
-    outputs[:] = 0
-    for block_id in dace.map[0:bn]:
-        for warp_id, thread_id in dace.map[0:32, 0:32]:
-            index = block_id * 1024 + warp_id * 32 + thread_id
-            index_x = dace.int64(index/640)
-            index_y = index % 640
-            value = warpReduce_sum(inputs[index_x, index_y])
-            if thread_id == 0:
-                outputs[index_x] += value
-    return outputs
-
+# multiple warps per row, multiple rows per block
 @dace.program
 def AB_MWPR_3(inputs: dace.float64[H, W], bn: dace.int64, ln: dace.int64, rpb: dace.int64, wpr: dace.int64):
     outputs = dace.ndarray([H], dtype=dace.float64)
@@ -62,15 +65,15 @@ def AB_MWPR_3(inputs: dace.float64[H, W], bn: dace.int64, ln: dace.int64, rpb: d
         shared = dace.ndarray([32,32], dtype=dace.float64, storage=dace.StorageType.GPU_Shared)
         for warp_id_x, warp_id_y, thread_id in dace.map[0:rpb, 0:wpr, 0:32]:
             value = dace.float64(0)
+            row_id = block_id * _rpb + warp_id_x
+            col_id = warp_id_y * 32 + thread_id
+            delta = _wpr * 32
             for i in dace.map[0:ln]:
-                value += inputs[block_id * _rpb + warp_id_x 
-                    ,i * _wpr * 32 + warp_id_y * 32 + thread_id]
-            shared[warp_id_x*wpr+warp_id_y, thread_id] = value
-        reduced = dace.ndarray([32], dtype=dace.float64, storage=dace.StorageType.GPU_Shared) # should be rpb instead of 32, but stupid dace can't work with variables here...
-        for warp_id_x, warp_id_y, thread_id in dace.map[0:rpb, 0:wpr, 0:32]:
-            reduced[warp_id_x] = warpReduce_sum(shared[warp_id_x * wpr + warp_id_y, thread_id])
-            if thread_id == 0:
-                outputs[block_id * rpb + warp_id_x] += reduced[warp_id_x]
+                value += inputs[row_id, col_id]
+                col_id += delta
+            reduced = warpReduce_sum(value)
+            if thread_id==0:
+                outputs[row_id] += reduced
     return outputs
 
 def test_1():
@@ -84,6 +87,16 @@ def test_1():
     c1 = np.sum(a1, axis=1)
     assert np.allclose(b1, c1)
 
+
+def test_1_s():
+    sdfg1s = AB_MWPR_1_special.to_sdfg()
+    sdfg1s.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+     
+    a1 = np.random.rand(h, w)
+    b1 = sdfg1s(H = h, W = w, inputs = a1)
+    c1 = np.sum(a1, axis=1)
+    assert np.allclose(b1, c1)
+
 def test_1_loop():
     sdfg = AB_MWPR_LOOP_1.to_sdfg()
     sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
@@ -94,23 +107,13 @@ def test_1_loop():
     b = sdfg(H = h, W = w, inputs = a, ln = loop_num)
     c = np.sum(a, axis=1)
     assert np.allclose(b, c)
-
-def test_2():
-    sdfg2 = AB_MWPR_2.to_sdfg()
-    sdfg2.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
-
-    block_num = h * w // 512
-
-    a2 = np.random.rand(h, w)
-    b2 = sdfg2(H = h, W = w, inputs = a2, bn = block_num)
-    c2 = np.sum(a2, axis=1)
-    assert np.allclose(b2, c2)
+    # print(b, c)
 
 def test_3():
     sdfg3 = AB_MWPR_3.to_sdfg()
     sdfg3.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
 
-    row_per_block = 2
+    row_per_block = w // 64
     warp_per_row = 32 // row_per_block
     block_num = h // row_per_block
     loop_num = w * row_per_block // 1024
@@ -126,15 +129,17 @@ if __name__ == '__main__':
     test_case = sys.argv[1]
     h, w = int(sys.argv[2]), int(sys.argv[3])
     
-    for i in range(16):
+    for i in range(4):
         if test_case ==  "1":
             if w > 1024:
                 test_1_loop()
+            elif w <= 32:
+                test_1_s()
             else:
                 test_1()
         elif test_case == "2":
-            test_2()
+            print("abandonded")
         elif test_case == "3":
             test_3()
         else:
-            raise Exception('invalid case number, only accept 1, 2, 3 and all')
+            raise Exception('invalid case number, only accept 1, 2, 3')
