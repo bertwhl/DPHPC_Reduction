f9d30e6f5e209761a23a63041cacbc35d4dedb0f
hazxel <steven.b.zhou@foxmail.com>
2021-12-18 01:11:10 +0100

    Test 2D finished, ready to plot

--- tests/BatchTest.py
+++ tests/BatchTest.py
@@ -11,7 +11,7 @@ def getTimeUs(str):
     matchObj = re.match(r'(.*)s', str)
     if matchObj:
         return 1000000 * float(matchObj.group(1))
-    raise Exception('cannot find desired contents')
+    raise Exception('error occurs when parsing time')
 
 def getAvgGPUTime(str):
     lines = str.splitlines()
@@ -22,10 +22,10 @@ def getAvgGPUTime(str):
         if isDataLine and line.find("CUDA memcpy") == -1 and line.find("assign_") == -1:
             item_list = list(filter(None, line.split(" ")))
             if len(item_list) < 4:
-                raise Exception('cannot find desired contents')
+                raise Exception('cannot find desired contents in line')
             return item_list[3]
-        if line.find("API calls:") != -1 or line.find("Error") != -1:
-            raise Exception('cannot find desired contents')
+        if isDataLine and (line.find("API calls:") != -1 or line.find("Error") != -1):
+            raise Exception('cannot find desired line')
     raise Exception('cannot find desired contents')
 
 def runWithTwoArgs(cmd, arg_list_1, arg_list_2):
@@ -38,6 +38,7 @@ def runWithTwoArgs(cmd, arg_list_1, arg_list_2):
                 print('{0:2d} {1:4d} {2:8f}'.format(a1, a2, time_us))
 
 def runWithThreeArgs(cmd, arg_list_1, arg_list_2, arg_list_3):
+    print("\n------- start running \"" + cmd.format("<arg_1>","<arg_2>","<arg_3>") + "\" -------\n")
     for a1 in arg_list_1:
         for a2 in arg_list_2:
             for a3 in arg_list_3:
@@ -45,10 +46,13 @@ def runWithThreeArgs(cmd, arg_list_1, arg_list_2, arg_list_3):
                 cmd_output = subprocess.getoutput(formated_cmd)
                 time_string = getAvgGPUTime(cmd_output)
                 time_us = getTimeUs(time_string)
-                print('{0:2d} {1:4d} {2:6d} {3:10f}'.format(a1, a2, a3, time_us))
+                print('{0:2d} {1:8d} {2:14d} {3:20f}'.format(a1, a2, a3, time_us))
+    print("\n------- finish running \"" + cmd.format("<arg_1>","<arg_2>","<arg_3>") + "\" -------\n")
 
 if __name__ == '__main__':
     subprocess.getoutput("conda activate dace")
-    runWithThreeArgs("nvprof python library/mwpr.py {} {} {}", [1], [128, 256], [512, 1024])
+    runWithThreeArgs("nvprof python tests/TestReduce2D.py {} {} {}", [1,3,4], range(16, 513, 16), [4096])
+    runWithThreeArgs("nvprof python tests/TestReduce2D.py {} {} {}", [6,7], range(4096, 12000, 512), [12])
+    runWithThreeArgs("nvprof python tests/TestReduce2D.py {} {} {}", [6,7], [10240], range(1,20))
 
  
---
+++ tests/TestReduce2D.py
@@ -0,0 +1,153 @@
+import sys
+import numpy as np
+import math
+from dace.transformation.interstate import GPUTransformSDFG
+
+sys.path.insert(0, "/home/zhoubo")
+from DPHPC_Reduction.library.Reduce2D import *
+
+
+# --------- AB Tests ---------
+
+
+# 1
+def test_rrm ():
+    sdfg = RowReduceMulti.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    BlockDefault = 64
+    if (h > BlockDefault * 32):
+        RowPerBlock = 32
+        WarpPerRow = 1
+    else:
+        RowPerBlock = (h + BlockDefault - 1) // BlockDefault
+        WarpPerRow = 32 // RowPerBlock
+    BlockNum = (h + RowPerBlock - 1) // RowPerBlock
+    loopNum = (w + WarpPerRow * 32 - 1) // (WarpPerRow * 32)
+
+    input = np.random.rand(h, w)
+    output = sdfg(H=h, W=w, inputs=input, 
+        gridDim_x=BlockNum,loopNum=loopNum, blockDim_z=RowPerBlock, blockDim_y=WarpPerRow)
+    ans = np.sum(input, axis=1)
+    assert np.allclose(ans, output)
+
+# 2
+def test_rrnl():
+    sdfg = RowReduceNoLoop.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    warp_num = math.ceil(w / 32) # w should be 1~32
+ 
+    input = np.random.rand(h, w)
+    output = sdfg(H=h, W=w, inputs=input, blockDim_y=warp_num)
+    ans = np.sum(input, axis=1)
+    assert np.allclose(ans, output)
+
+# 3
+def test_rrg():
+    sdfg = RowReduceGlobal.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    if (h==64):
+        BlockPerRow = 1
+    else:
+        BlockPerRow = 2
+    loop_num = (w+BlockPerRow*1024-1)//(BlockPerRow*1024)
+
+    input = np.random.rand(h, w)
+    output = sdfg(H=h, W=w, inputs=input, gridDim_y=BlockPerRow, loopNum=loop_num)
+    ans = np.sum(input, axis=1)
+    assert np.allclose(ans, output)
+
+# 4
+def test_rrs():
+    sdfg = RowReduceShared.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    BlockDefault = 64
+    grid_dim = max(1, math.floor(BlockDefault / h))
+    loop_num = math.ceil(w / grid_dim / 1024)
+
+    input = np.random.rand(h, w)
+    output = sdfg(H=h, W=w, inputs=input, gridDim_y=grid_dim, loopNum=loop_num)
+    ans = np.sum(input, axis=1)
+    assert np.allclose(ans, output)
+
+# 5
+def test_rrn():
+    sdfg = RowReduceNarrow.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    ThreadPerBlock = 256
+    BlockNum = (h+ThreadPerBlock-1)//ThreadPerBlock
+
+    input = np.random.rand(h, w)
+    output = sdfg(H=h, W=w, inputs=input, gridDim_x=BlockNum, blockDim_x=ThreadPerBlock)
+    ans = np.sum(input, axis=1)
+    assert np.allclose(ans, output)
+
+
+# --------- BA Tests ---------
+
+
+# 6
+def test_cr():
+    sdfg = ColReduce.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    BlockPerRow = 4
+    BlockPerColumn = 16
+    ThreadPerBlock = math.ceil(w / BlockPerRow)
+    loopNum = (h+BlockPerColumn-1)//BlockPerColumn
+
+    inputs = np.random.rand(h, w)
+    outputs = sdfg(H=h, W=w, inputs=inputs, gridDim_x=BlockPerRow, gridDim_y=BlockPerColumn, blockDim_x=ThreadPerBlock, loopNum=loopNum)
+    compared = np.sum(inputs, axis=0)
+    assert np.allclose(outputs, compared)
+
+# 7
+def test_crn():
+    sdfg = ColReduceNarrow.to_sdfg()
+    sdfg.apply_transformations(GPUTransformSDFG, {'sequential_innermaps': False})
+
+    BlockDefault = 64
+    BlockMax = (h+32-1)//32
+    if BlockDefault > BlockMax:
+        BlockPerColumn = BlockMax
+        loopNum = 1
+    else:
+        BlockPerColumn = BlockDefault
+        loopNum = (h+32*BlockPerColumn-1)//(32*BlockPerColumn)
+
+    inputs = np.random.rand(h, w)
+    outputs = sdfg(H=h, W=w, inputs=inputs, gridDim_x=BlockPerColumn, loopNum=loopNum)
+    compared = np.sum(inputs, axis=0)
+    assert np.allclose(outputs, compared)    
+
+
+# --------- Run Tests ---------
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        raise Exception('only accept 3 arguments')
+    test_case = sys.argv[1]
+    h, w = int(sys.argv[2]), int(sys.argv[3])
+    
+    for i in range(1):
+        if test_case ==  "1":
+            test_rrm()
+        elif test_case == "2":
+            test_rrnl()
+        elif test_case == "3":
+            test_rrg()
+        elif test_case == "4":
+            test_rrs()
+        elif test_case == "5":
+            test_rrn()
+        elif test_case == "6":
+            test_cr()
+        elif test_case == "7":
+            test_crn()
+        else:
+            raise Exception('invalid case number, only accept 1, 2, 3, 4, 5, 6, 7')
